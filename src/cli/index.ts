6591ea6c2f9583bcea845f658e995a77d26446ed
Ryan Hiebert <ryan@ryanhiebert.com>
2017-08-20 12:53:01 -0500

    Stub out serve command

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,10 +1,13 @@
 import * as chalk from 'chalk'
 import build from './build'
+import serve from './serve'
 
 export default function main ([nodeArg, scriptArg, commandArg, ...args]: string[]) {
   console.log(chalk.white.bold('DerWeg version 0.0.0'))
   if (commandArg === 'build') {
     build(args)
+  } else if (commandArg === 'serve') {
+    serve(args)
   } else {
     console.log(chalk.red(`Unknown command '${commandArg}'`))
   }
---
+++ src/cli/serve.ts
@@ -0,0 +1,6 @@
+import * as chalk from 'chalk'
+
+export default function serve (args: string[]) {
+  console.log(chalk.green('Serving the project (not really).'))
+  console.log(chalk.green('exiting.'))
+}
